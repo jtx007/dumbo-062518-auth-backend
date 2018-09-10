692c318aab2e0dcdbc4153eb187f62bcfd0e8838
perpepajn25 <perpepajn25@gmail.com>
2018-09-10 17:40:05 -0400

    fixed

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AuthController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:login]
 
   def login
     @user = User.find_by(username: auth_params['username'])
